7c324c0ff67368cef7d6e21d4ed77553e76b8e96
Tim <tim@brightsidedeveloper.com>
2022-11-12 12:47:35 -0600

    NPM Dependencies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import tw from "twin.macro";
+
 function App() {
-  return <>Hi</>;
+  return <Body>Hi</Body>;
 }
 
 export default App;
+
+const Body = tw.div`bg-red-500 w-full  text-7xl text-white`;
